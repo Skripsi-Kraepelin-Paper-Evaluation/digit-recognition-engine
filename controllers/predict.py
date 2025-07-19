22ad3e705cedc612105fc7b880e78ba9b29079b4
Hadekha Erfadila Fitra <hadekha.fitra@pactindo.com>
2025-07-19 14:58:19 +0700

    adjustment

--- controllers/predict.py
+++ controllers/predict.py
@@ -30,7 +30,7 @@ class PredictHandler:
         self.persistent_path = persistent_path
 
     def _create_obj(self, predicted_digit, accuracy, row, col, blank, is_question):
-        model_class = question.PredictedDigitQuestion if is_question else answer.PredictedDigitAnswer
+        model_class = answer.PredictedDigitAnswer
         return model_class(
             digit=predicted_digit if not blank else None,
             accuracy=accuracy if not blank else None,
@@ -91,13 +91,58 @@ class PredictHandler:
 
     def handle_predict(self, filename):
         base_path = f'{self.persistent_path}/roi_result/{filename}'
-        questions = self.process_images(os.path.join(base_path, 'questions'), is_question=True)
+        # questions is fixed
+        # 40 x 56
+        questions_fixed = [
+            [9,7,2,9,5,6,6,7,3,6,4,8,3,9,9,5,4,4,9,2,2,8,6,5,5,7,8,9,3,3,4,0,8,2,7,4,3,6,5,9,5,8,6,4,9,3,4,6,7,9,6,9,2,7,5,9],
+            [9,9,8,4,4,6,5,2,7,7,8,9,3,3,7,1,6,2,5,7,9,4,2,8,2,7,7,3,5,2,3,2,4,7,5,8,6,9,8,4,5,3,7,4,6,3,8,8,7,6,5,9,8,9,4,4],
+            [0,3,6,8,3,4,5,7,6,3,5,9,4,2,4,7,9,2,5,6,7,4,3,2,6,9,5,8,8,7,5,3,8,6,6,4,9,7,2,8,5,5,4,8,3,9,9,6,8,2,2,0,6,3,3,8],
+            [4,2,6,4,3,7,9,6,5,4,8,6,7,7,2,3,3,4,9,5,2,7,6,8,8,9,2,4,5,3,9,8,5,5,6,3,2,9,4,4,6,2,2,8,7,5,9,9,7,3,8,4,6,2,3,4],
+            [6,2,7,9,5,5,7,8,5,4,9,7,7,2,4,3,8,6,9,2,2,8,7,3,9,4,4,5,6,6,3,2,9,8,8,3,5,1,7,8,2,5,3,6,6,9,3,5,8,4,7,6,7,2,5,9],
+            [8,8,4,5,3,8,7,7,2,9,6,5,4,4,7,6,2,8,9,9,3,2,0,2,5,2,3,6,4,8,2,6,7,4,6,5,9,6,8,9,9,3,4,7,5,8,4,2,3,3,7,8,4,8,3,5],
+            [7,8,9,2,2,3,7,1,7,4,2,5,9,7,3,6,9,4,6,8,3,3,5,7,3,4,8,5,6,3,7,8,2,7,5,5,8,6,4,9,2,6,6,7,9,5,2,2,4,3,9,7,9,8,2,2],
+            [7,2,6,8,8,3,4,8,3,5,2,7,6,4,3,6,5,3,9,8,4,7,5,4,2,9,6,6,3,8,5,5,9,7,4,6,9,3,3,2,8,7,9,9,5,6,2,4,4,5,7,7,6,2,8,8],
+            [7,8,8,6,3,9,3,4,6,8,5,5,2,7,4,4,9,7,6,6,2,3,5,4,7,7,2,9,4,5,3,3,6,9,9,8,4,2,8,7,3,8,0,4,9,4,8,2,5,8,6,7,8,8,3,6],
+            [2,8,6,4,2,7,7,8,3,9,5,4,4,6,7,3,3,5,2,9,8,8,4,3,6,9,1,9,5,9,6,7,5,6,3,2,5,7,9,6,4,8,9,2,6,5,8,2,2,4,3,2,6,8,2,4],
+            [4,4,3,5],
+            [],
+            [],
+            [],
+            [],
+            [],
+            [],
+            [],
+            [],
+            [],
+            [],
+            [],
+            [],
+            [],
+            [],
+            [],
+            [],
+            [],
+            [],
+            [],
+            [],
+            [],
+            [],
+            [],
+            [],
+            [],
+            [],
+            [],
+            [],
+            []
+        ]
         answers = self.process_images(os.path.join(base_path, 'answers'), is_question=False)
+
+        # sort answers by first column, first rows
 
 
         result = {
             'filename': filename,
-            'questions': [self._serialize_obj(q) for q in questions],
+            'questions': questions_fixed,
             'answers': [self._serialize_obj(a) for a in answers],
             'total_questions': len(questions),
             'total_answers': len(answers)
